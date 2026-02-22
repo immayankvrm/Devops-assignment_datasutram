28dc9754f9c035d1b32fc95d163bc715a2a6a32d
mayank <vinverma564@gmail.com>
2026-02-23 00:15:57 +0530

    first-commit

---
+++ devops-sample-fast/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Sample FastAPI App")
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok",
+        "service": "fastapi-app"
+    }
+
+@app.get("/hello")
+def hello():
+    return {
+        "message": "Hello from FastAPI v2"
+    }
+
+@app.get("/products/{product_id}")
+def get_product(product_id: int):
+    return {
+        "id": product_id,
+        "name": "Sample Product",
+        "price": 999
+    }
